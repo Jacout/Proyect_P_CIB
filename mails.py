f16501926ec85d354f33cfe45afeb9ab11bf559c
supercapish <166556755+supercapish@users.noreply.github.com>
2024-04-24 14:47:10 -0600

    se modifico los puertos y los correos

--- mails.py
+++ mails.py
@@ -1,13 +1,16 @@
 import requests
 import re
-
-response = requests.get(target)
-if response.status_code != 200:
-    exit()
+def mails():
+    response = requests.get(target)
+    if response.status_code != 200:
+        exit()
 #obtiene las urls
-regExMail = r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"
-new_emails = set(re.findall(regExMail, response.text, re.I))
+    regExMail = r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"
+    correos = set(re.findall(regExMail, response.text, re.I))
 
-file = open("correos.txt","w")
-for i in new_emails:
-    file.write(i + "\n")
+    file = open("correos.txt","w")
+    for i in correos:
+        file.write(i + "\n")
+     
+    file.close()
+    
